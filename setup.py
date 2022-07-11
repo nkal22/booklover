c100efb8708e936152a877c7e3b4347c25fc7e16
Nick Kalinowski <72101251+nkal22@users.noreply.github.com>
2022-07-11 09:32:17 -0400

    Add files via upload

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='booklover',
+      version='0.1',
+      description='I love books',
+      url='https://github.com/nkal22/booklover/tree/main/booklover',
+      author='Nick Kalinowski',
+      author_email='njk5yr@virginia.edu',
+      license='MIT',
+      packages=['booklover'])
